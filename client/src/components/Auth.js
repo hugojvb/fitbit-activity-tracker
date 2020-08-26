8c01c012dfb01cc79417d163b7fc0372637825f8
Hugo <hugo.j.v.batista@outlook.com>
2020-08-26 17:35:45 +0100

    added auth jsx

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+import "../style/auth.css";
 
 export default class Auth extends Component {
-  static propTypes = {
-    prop: PropTypes,
-  };
-
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <form>
+          <fieldset>
+            <legend>Sign in</legend>
+            <div>
+              <label htmlfor="name" value="name">
+                Name:
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Please enter your name"
+              />
+            </div>
+            <div>
+              <label htmlfor="email" value="email">
+                E-mail:
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Please enter your email"
+              />
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    );
   }
 }
