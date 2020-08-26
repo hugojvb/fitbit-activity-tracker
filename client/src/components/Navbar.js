f6a303018902b59f1963a93e55a6b2741c567574
Hugo <hugo.j.v.batista@outlook.com>
2020-08-26 23:21:33 +0100

    changed logo and title

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,8 +4,9 @@ import "../style/navbar.css";
 const Navbar = () => {
   return (
     <nav id="navbar">
-      <i id="logo" className="fas fa-dumbbell fa-1x" />
-      <h4 id="title">Workout Tracker</h4>
+      {/* <i id="logo" className="fas fa-dumbbell fa-1x" /> */}
+      <img id="logo" src={require("../assets/logo.png")} alt="fitbit logo" />
+      <h4 id="title">Fitbit Activity Tracker</h4>
       <h5 className="signout">Sign Out</h5>
     </nav>
   );
