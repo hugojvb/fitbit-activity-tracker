6851353e3553bfdf40b0991ccad32272c37fc3ab
Hugo <hugo.j.v.batista@outlook.com>
2020-08-26 16:23:59 +0100

    Added background

--- client/src/Grid.js
+++ client/src/Grid.js
@@ -1,4 +1,5 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
+import "./style/grid.css";
 
 class Grid extends Component {
   constructor() {
@@ -10,10 +11,56 @@ class Grid extends Component {
 
   render() {
     return (
-      <Fragment className="grid-container">
-        <div className="card1"></div>
-        <div className="card1"></div>
-      </Fragment>
+      <div className="grid-container">
+        <div className="bg1">
+          <h2>2/5</h2>
+          <p>Goals</p>
+        </div>
+        <div className="bg1">
+          <h2>265</h2>
+          <p>Calories Out</p>
+        </div>
+        <div className="bg2">
+          <i className="fas fa-chart-line fa-2x" />
+          <p>BMR</p>
+        </div>
+        <div className="bg1">
+          <i className="fas fa-shoe-prints fa-2x" />
+          <p>Steps</p>
+        </div>
+        <div className="bg1">
+          <i className="fas fa-mountain fa-2x" />
+          <p>Elevation</p>
+        </div>
+        <div className="bg2">
+          <i className="fas fa-running fa-2x" />
+          <p>Distance</p>
+        </div>
+        <div className="bg1">
+          <h2>25</h2>
+          <p>(Body Fat %)</p>
+        </div>
+        <div className="bg2">
+          <h2>75</h2>
+          <p>(Kg)</p>
+        </div>
+        <div className="bg2">
+          <i className="fas fa-utensils fa-2x" />
+          <p>Calorie Intake</p>
+        </div>
+        <div className="bg1">
+          <h2>50</h2>
+          <p>(mins. Active)</p>
+        </div>
+        <div className="bg2">
+          <i className="fas fa-heartbeat fa-2x" />
+          <p>Heart Rate</p>
+        </div>
+        <div className="bg2">
+          <i className="fas fa-bed fa-2x" />
+          <p>Sleep</p>
+        </div>
+      </div>
     );
   }
 }
