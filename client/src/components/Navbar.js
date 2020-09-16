a4ba553abf750c7dd1aab4fc54d9f9c0e8438e8b
Hugo <hugo.j.v.batista@outlook.com>
2020-09-16 18:22:30 +0100

    Added signout button's functionality

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../style/navbar.css";
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
     <nav id="navbar">
       <img id="logo" src={require("../assets/logo.png")} alt="fitbit logo" />
       <h4 id="title">Fitbit Activity Tracker</h4>
+
       <a href="http://localhost:3000/" className="signout">
         Sign Out
       </a>
