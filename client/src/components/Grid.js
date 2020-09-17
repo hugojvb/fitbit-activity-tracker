34d5b59f576f3f0b119c96b200518c254e85043d
Hugo <hugo.j.v.batista@outlook.com>
2020-09-17 14:18:08 +0100

    Changing whole app to hooks and context

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -1,101 +1,87 @@
-import React, { Component } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "../style/grid.css";
 import * as queryString from "query-string";
 import Spinner from "./Spinner";
+import Context from "../context/context";
 
-class Grid extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      loading: true,
-      date: this.formatDate(new Date()),
-    };
-    this.parsed = queryString.parse(props.location.hash);
-    console.log(this.parsed);
-  }
+const Grid = (props) => {
+  const [loading, setLoading] = useState({ loading: false });
+  const context = useContext(Context);
 
-  async componentDidMount() {
+  async function getData() {
+    const parsed = queryString.parse(props.location.hash);
+    console.log(parsed);
     const res = await fetch(
-      `https://api.fitbit.com/1/user/${this.parsed.user_id}/activities/date/${this.state.date}.json`,
+      `https://api.fitbit.com/1/user/${parsed.user_id}/activities/date/${context.date}.json`,
       {
         headers: {
-          Authorization: `${this.parsed.token_type} ${this.parsed.access_token}`,
+          Authorization: `${parsed.token_type} ${parsed.access_token}`,
         },
       }
     );
     const data = await res.json();
+    setLoading();
     console.log(data);
-    this.setState({ loading: false });
   }
 
-  formatDate = (date) => {
-    var d = new Date(date),
-      month = "" + (d.getMonth() + 1),
-      day = "" + d.getDate(),
-      year = d.getFullYear();
+  useEffect(() => {
+    getData();
+  });
 
-    if (month.length < 2) month = "0" + month;
-    if (day.length < 2) day = "0" + day;
-
-    return [year, month, day].join("-");
-  };
-
-  render() {
-    return this.state.loading === false ? (
-      <div className="grid-container">
-        <div className="bg1">
-          <h2>2/5</h2>
-          <p>Goals</p>
-        </div>
-        <div className="bg1">
-          <h2>265</h2>
-          <p>Calories Out</p>
-        </div>
-        <div className="bg2">
-          <i className="fas fa-chart-line fa-2x" />
-          <p>BMR</p>
-        </div>
-        <div className="bg1">
-          <i className="fas fa-shoe-prints fa-2x" />
-          <p>Steps</p>
-        </div>
-        <div className="bg1">
-          <i className="fas fa-mountain fa-2x" />
-          <p>Elevation</p>
-        </div>
-        <div className="bg2">
-          <i className="fas fa-running fa-2x" />
-          <p>Distance</p>
-        </div>
-        <div className="bg1">
-          <h2>25</h2>
-          <p>(Body Fat %)</p>
-        </div>
-        <div className="bg2">
-          <h2>75</h2>
-          <p>(Kg)</p>
-        </div>
-        <div className="bg2">
-          <i className="fas fa-utensils fa-2x" />
-          <p>Calorie Intake</p>
-        </div>
-        <div className="bg1">
-          <i className="fas fa-history fa-2x" />
-          <p>Activity</p>
-        </div>
-        <div className="bg2">
-          <i className="fas fa-heartbeat fa-2x" />
-          <p>Heart Rate</p>
-        </div>
-        <div className="bg2">
-          <i className="fas fa-bed fa-2x" />
-          <p>Sleep</p>
-        </div>
+  return loading === false ? (
+    <div className="grid-container">
+      <div className="bg1">
+        <h2>2/5</h2>
+        <p>Goals</p>
       </div>
-    ) : (
-      <Spinner />
-    );
-  }
-}
+      <div className="bg1">
+        <h2>265</h2>
+        <p>Calories Out</p>
+      </div>
+      <div className="bg2">
+        <i className="fas fa-chart-line fa-2x" />
+        <p>BMR</p>
+      </div>
+      <div className="bg1">
+        <i className="fas fa-shoe-prints fa-2x" />
+        <p>Steps</p>
+      </div>
+      <div className="bg1">
+        <i className="fas fa-mountain fa-2x" />
+        <p>Elevation</p>
+      </div>
+      <div className="bg2">
+        <i className="fas fa-running fa-2x" />
+        <p>Distance</p>
+      </div>
+      <div className="bg1">
+        <h2>25</h2>
+        <p>(Body Fat %)</p>
+      </div>
+      <div className="bg2">
+        <h2>75</h2>
+        <p>(Kg)</p>
+      </div>
+      <div className="bg2">
+        <i className="fas fa-utensils fa-2x" />
+        <p>Calorie Intake</p>
+      </div>
+      <div className="bg1">
+        <i className="fas fa-history fa-2x" />
+        <p>Activity</p>
+      </div>
+      <div className="bg2">
+        <i className="fas fa-heartbeat fa-2x" />
+        <p>Heart Rate</p>
+      </div>
+      <div className="bg2">
+        <i className="fas fa-bed fa-2x" />
+        <p>Sleep</p>
+      </div>
+    </div>
+  ) : (
+    <Spinner />
+  );
+};
 
 export default Grid;
--- client/src/context/state.js
+++ client/src/context/state.js
@@ -4,8 +4,22 @@ import reducer from "./reducer";
 import { LOGGED_IN } from "./types";
 
 const State = (props) => {
+  const formatDate = (date) => {
+    var d = new Date(date),
+      month = "" + (d.getMonth() + 1),
+      day = "" + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return [year, month, day].join("-");
+  };
+
   const initialState = {
     logged_in: false,
+    loading: true,
+    date: formatDate(new Date()),
   };
 
   const [state, dispatch] = useReducer(reducer, initialState);
