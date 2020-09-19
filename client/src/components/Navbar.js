25354d121b5eb301e46910cf17859a8528f7be2d
Hugo <hugo.j.v.batista@outlook.com>
2020-09-19 23:01:38 +0100

    Added activeScore to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,16 +5,28 @@ import Context from "../context/context";
 
 const Navbar = (props) => {
   const context = useContext(Context);
+  console.log(context.activity.summary);
 
   return (
     <nav id="navbar">
       <img id="logo" src={logo} alt="fitbit logo" />
       <h4 id="title">Fitbit Activity Tracker</h4>
-      {context.logged_in && (
-        <a href="http://localhost:3000/" className="signout">
-          <h4>Log Out</h4>
-        </a>
-      )}
+      <ul>
+        <li>
+          {context.activity.summary && (
+            <h6 className="active">
+              Activity Score: {context.activity.summary.activeScore}
+            </h6>
+          )}
+        </li>
+        <li>
+          {context.logged_in && (
+            <a href="http://localhost:3000/" className="signout">
+              <h4>Log Out</h4>
+            </a>
+          )}
+        </li>
+      </ul>
     </nav>
   );
 };
