0f71a22f6cd0e5bdc394116043e3f72237aef508
Hugo <hugo.j.v.batista@outlook.com>
2020-09-22 16:40:21 +0100

    Added modal reducers

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -91,6 +91,116 @@ export default (state, action) => {
         ...state,
         showGoalsModal: false,
       };
+    case OPEN_CALORIESOUT_MODAL:
+      return {
+        ...state,
+        showCaloriesOutModal: true,
+      };
+    case CLOSE_CALORIESOUT_MODAL:
+      return {
+        ...state,
+        showCaloriesOutModal: false,
+      };
+    case OPEN_CALORIESBMR_MODAL:
+      return {
+        ...state,
+        showCaloriesBMRModal: true,
+      };
+    case CLOSE_CALORIESBMR_MODAL:
+      return {
+        ...state,
+        showCaloriesBMRModal: false,
+      };
+    case OPEN_STEPS_MODAL:
+      return {
+        ...state,
+        showStepsModal: true,
+      };
+    case CLOSE_STEPS_MODAL:
+      return {
+        ...state,
+        showStepsModal: false,
+      };
+    case OPEN_BMI_MODAL:
+      return {
+        ...state,
+        showBMIModal: true,
+      };
+    case CLOSE_BMI_MODAL:
+      return {
+        ...state,
+        showBMIModal: false,
+      };
+    case OPEN_DISTANCE_MODAL:
+      return {
+        ...state,
+        showDistanceModal: true,
+      };
+    case CLOSE_DISTANCE_MODAL:
+      return {
+        ...state,
+        showDistanceModal: false,
+      };
+    case OPEN_CALORIEINTAKE_MODAL:
+      return {
+        ...state,
+        showCalorieIntakeModal: true,
+      };
+    case CLOSE_CALORIEINTAKE_MODAL:
+      return {
+        ...state,
+        showCalorieIntakeModal: false,
+      };
+    case OPEN_WEIGHT_MODAL:
+      return {
+        ...state,
+        showWeightModal: true,
+      };
+    case CLOSE_WEIGHT_MODAL:
+      return {
+        ...state,
+        showWeightModal: false,
+      };
+    case OPEN_BODYFAT_MODAL:
+      return {
+        ...state,
+        showBodyFatModal: true,
+      };
+    case CLOSE_BODYFAT_MODAL:
+      return {
+        ...state,
+        showBodyFatModal: false,
+      };
+    case OPEN_ACTIVITY_MODAL:
+      return {
+        ...state,
+        showActivityModal: true,
+      };
+    case CLOSE_ACTIVITY_MODAL:
+      return {
+        ...state,
+        showActivityModal: false,
+      };
+    case OPEN_HEARTRATE_MODAL:
+      return {
+        ...state,
+        showHeartRateModal: true,
+      };
+    case CLOSE_HEARTRATE_MODAL:
+      return {
+        ...state,
+        showHeartRateModal: false,
+      };
+    case OPEN_SLEEP_MODAL:
+      return {
+        ...state,
+        showSleepModal: true,
+      };
+    case CLOSE_SLEEP_MODAL:
+      return {
+        ...state,
+        showSleepModal: false,
+      };
     default:
       return state;
   }
