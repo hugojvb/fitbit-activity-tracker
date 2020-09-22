a8067a89c0846256fa190d5be76d30cd9ef87a66
Hugo <hugo.j.v.batista@outlook.com>
2020-09-22 19:53:34 +0100

    Added goals modal with grid

--- client/src/components/GoalsModal.js
+++ client/src/components/GoalsModal.js
@@ -6,6 +6,9 @@ import "react-responsive-modal/styles.css";
 const GoalsModal = () => {
   const context = useContext(Context);
   const { showGoalsModal, closeGoalsModal } = context;
+  const { goals } = context.activity;
+
+  console.log(context.activity.goals);
 
   return (
     <div onClick={(e) => e.stopPropagation()}>
@@ -17,7 +20,19 @@ const GoalsModal = () => {
         center
         classNames={{ modal: "modal_container", closeButton: "close_button" }}
       >
-        <p>context.activity.goals</p>
+        <p>Goals</p>
+        <div className="modal_table">
+          <p>
+            {Object.keys(goals).map((key) => (
+              <li>{key}</li>
+            ))}
+          </p>
+          <p>
+            {Object.values(goals).map((value) => (
+              <li>{value}</li>
+            ))}
+          </p>
+        </div>
       </Modal>
     </div>
   );
